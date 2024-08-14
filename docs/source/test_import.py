b335332045ad4e2a339f4983d559ca6f97a08a51
zihan.li <li-zihan@alumni.pku.edu.cn>
2024-08-14 11:23:34 +0100

    delete useless dir

--- docs/source/test_import.py
+++
@@ -1,8 +0,0 @@
-# docs/source/test_import.py
-try:
-    import montecarlo.handler
-    import montecarlo.processor
-    import montecarlo.driver
-    print("Modules imported successfully")
-except ImportError as e:
-    print(f"ImportError: {e}")
